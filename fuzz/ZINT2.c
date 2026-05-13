f1c41bf8fc5c4d2afd28cb80dd8ccbbb035ef54e
Victor Grishchenko <gritzko@replicated.live>
2026-05-13 07:14:47 +0200

    abc: fix the fuzz test build

--- fuzz/ZINT2.c
+++ fuzz/ZINT2.c
@@ -1,16 +1,17 @@
 #include "ZINT.h"
 
 #include "TEST.h"
+#define MAX_LEN 1024
 
 FUZZ(u64, ZINT2fuzz) {
     sane(1);
-    if ($empty(input)) done;
+    if ($empty(input) || $len(input)>MAX_LEN) done;
     a_dup(u64c, src, input);
-    aBpad(u8, pad, $len(input) * 9 + 8);
+    aBpad(u8, pad, MAX_LEN * 9 + 8);
     u8sp into = u8bIdle(pad);
     call(ZINTu8sFeedBlocked, into, src);
     u8csp data = u8bDataC(pad);
-    aBpad(u64, out, $len(input));
+    aBpad(u64, out, MAX_LEN);
     u64sp drain = u64bIdle(out);
     call(ZINTu8sDrainBlocked, data, drain);
     a_dup(u64c, cmp, u64bDataC(out));
